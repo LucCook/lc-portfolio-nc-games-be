2c2591a1500ca545338d62e0384b75907e26364a
LucCook <luc_cook@hotmail.co.uk>
2022-12-12 15:32:50 +0000

    this commit will add the controller and model for new endpoint get /api/reviews/review_id, error handlers for PSQL and custom errors, and all associated testing

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const { getCategories } = require("./controllers/categories.controllers")
-const { getReviews } = require("./controllers/reviews.controllers")
-const { handle500, handle404 } = require("./errors/handlers")
+const { getReviews, getReviewById } = require("./controllers/reviews.controllers")
+const { handle500, handle404, handleCustom, handle400 } = require("./errors/handlers")
 
 const app = express()
 
 app.get("/api/categories", getCategories)
 app.get("/api/reviews", getReviews)
+app.get("/api/reviews/:review_id", getReviewById)
 
 app.all("*", handle404)
 
+app.use(handleCustom)
+app.use(handle400)
 app.use(handle500)
 
 module.exports = app
--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,7 +1,21 @@
-const db = require("../db/connection.js")
+const db = require("../db/connection.js");
 
 exports.selectReviews = () => {
-    return db.query("SELECT owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, designer, COUNT(comments.comment_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at DESC").then(({rows}) => {
-        return rows
-    })
-}
+  return db
+    .query(
+      "SELECT owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, designer, COUNT(comments.comment_id)::INT AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at DESC"
+    )
+    .then(({ rows: reviews }) => {
+      return reviews;
+    });
+};
+
+exports.selectReviewById = (reviewId) => {
+  return db
+    .query("SELECT * FROM reviews WHERE review_id = $1", [reviewId])
+    .then(({ rows: [review] }) => {
+      if (review === undefined) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      } else return review;
+    });
+};
