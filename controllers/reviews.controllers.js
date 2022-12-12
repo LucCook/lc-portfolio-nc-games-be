80d66e257c395ecf705d16685a9fc4cc5cb7e26e
LucCook <luc_cook@hotmail.co.uk>
2022-12-12 14:44:48 +0000

    this commit will addd a catch block on the getReviews controller

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -3,5 +3,5 @@ const { selectReviews } = require("../models/reviews.models")
 exports.getReviews = (req, res, next) => {
     selectReviews().then((reviews) => {
         res.status(200).send({reviews})
-    })
+    }).catch(next)
 }
