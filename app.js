8cd1012b047ea890bc4cdb931654583aafb5b091
LucCook <luc_cook@hotmail.co.uk>
2022-12-13 14:06:04 +0000

    this commit is just for formatting

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const express = require("express")
-const { getCategories } = require("./controllers/categories.controllers")
-const { getCommentsByReviewId, postCommentByReviewId  } = require("./controllers/comments.controllers")
-const { getReviews, getReviewById , patchReview } = require("./controllers/reviews.controllers")
-const { getUsers } = require("./controllers/users.controllers")
-const { handle500, handle404, handleCustom, handle400 } = require("./errors/handlers")
+const express = require("express");
+const { getCategories } = require("./controllers/categories.controllers");
+const {
+  getCommentsByReviewId,
+  postCommentByReviewId,
+} = require("./controllers/comments.controllers");
+const {
+  getReviews,
+  getReviewById,
+  patchReview,
+} = require("./controllers/reviews.controllers");
+const { getUsers } = require("./controllers/users.controllers");
+const {
+  handle500,
+  handle404,
+  handleCustom,
+  handle400,
+} = require("./errors/handlers");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.get("/api/users", getUsers)
-app.get("/api/categories", getCategories)
-app.get("/api/reviews", getReviews)
-app.get("/api/reviews/:review_id", getReviewById)
-app.get("/api/reviews/:review_id/comments", getCommentsByReviewId)
-app.post("/api/reviews/:review_id/comments", postCommentByReviewId)
+app.use(express.json());
+app.get("/api/users", getUsers);
+app.get("/api/categories", getCategories);
+app.get("/api/reviews", getReviews);
+app.get("/api/reviews/:review_id", getReviewById);
+app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
+app.post("/api/reviews/:review_id/comments", postCommentByReviewId);
 
+app.all("*", handle404);
 
-app.all("*", handle404)
+app.use(handleCustom);
+app.use(handle400);
+app.use(handle500);
 
-app.use(handleCustom)
-app.use(handle400)
-app.use(handle500)
-
-module.exports = app
+module.exports = app;
