5b69191632b2cc187b3d6ae0a278b258b556ef31
LucCook <luc_cook@hotmail.co.uk>
2022-12-13 16:42:38 +0000

    this commit will clarify sort order in testing and test for filtering by valid category that has no associated reviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -73,7 +73,15 @@ describe("GET api/reviews?queries", () => {
       })
     })
   })
-  test("200: should respond with an array of objects, sorted by the column defined by the sort_by parameter", () => {
+  test("200: should respond with an empty array when passed a category query parameter that exists in the category table but has no associated reviews in the review table", () => {
+    return request(app)
+    .get("/api/reviews?category=children's_games")
+    .expect(200)
+    .then(({body: {reviews}}) => {
+      expect(reviews.length).toBe(0)
+    })
+  })
+  test("200: should respond with an array of objects, sorted by the column defined by the sort_by parameter, in descending order", () => {
     return request(app)
     .get("/api/reviews?sort_by=votes")
     .expect(200)
