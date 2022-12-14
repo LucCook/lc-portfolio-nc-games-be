bebd5892d4c76fa7342908faabe4c68d2db7ed84
LucCook <luc_cook@hotmail.co.uk>
2022-12-14 10:01:46 +0000

    this commit will add a catch block to getApiEndpoints controller

--- controllers/utility.controllers.js
+++ controllers/utility.controllers.js
@@ -3,5 +3,5 @@ const fs = require("fs/promises");
 exports.getApiEndpoints = (req, res, next) => {
   fs.readFile(`${__dirname}/../endpoints.json`).then((endpoints) => {
     res.status(200).send(endpoints);
-  });
+  }).catch(next)
 };
