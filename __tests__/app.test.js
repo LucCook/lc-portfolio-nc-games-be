cedf846e5411128a3e457dd03d9ddb454cff899b
LucCook <luc_cook@hotmail.co.uk>
2022-12-14 09:56:41 +0000

    this commit will add functionality and testing for get /api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const seed = require("../db/seeds/seed.js");
 const request = require("supertest");
 const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
+const fs = require('fs/promises')
 
 afterAll(() => db.end());
 
@@ -448,6 +449,15 @@ describe("DELETE /api/comments/:comment_id", () => {
         expect(msg).toBe("bad request");
       });
   });
+});
+
+describe('GET /api', () => {
+  test("200: should respond with a JSON object of available endpoints", () => {
+    return Promise.all([fs.readFile(`${__dirname}/../endpoints.json`), request(app).get("/api").expect(200)])
+    .then(([jsonFile, {body : jsonResponse}]) => {
+      expect(jsonFile).toEqual(jsonResponse)
+    })
+  })
 });
 
 describe("request invalid path", () => {
---
+++ controllers/utility.controllers.js
@@ -0,0 +1,7 @@
+const fs = require("fs/promises");
+
+exports.getApiEndpoints = (req, res, next) => {
+  fs.readFile(`${__dirname}/../endpoints.json`).then((endpoints) => {
+    res.status(200).send(endpoints);
+  });
+};
