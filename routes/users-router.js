0fed4b675c38df3a135a82fbec2613e4134ddacb
LucCook <luc_cook@hotmail.co.uk>
2022-12-15 14:00:14 +0000

    this commit will update with refactor to express routers

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getUsers } = require("../controllers/users.controllers");
+const { getUsers, getUserByUsername } = require("../controllers/users.controllers");
 const usersRouter = express.Router();
 
 usersRouter.route("/").get(getUsers);
+usersRouter.route("/:username").get(getUserByUsername)
 
 module.exports = usersRouter;
